85f35ddae3cc5ce60bab01a7607a2fa386a7eca8
Lloyd Park <lloyd@Lloyds-iMac.local>
2016-12-08 17:22:31 -0800

    Solution added

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,38 @@
 $(function() {
 
   // your code will go here
-
+  $.ajax({
+  	url: 'https://www.codeschool.com/users/giyen9088.json',
+  	dataType: 'jsonp',
+  	success: function(response) {
+  		console.log(response.courses.completed);
+  		addCourses(response.courses.completed);
+  	}
+  });
 });
+
+
+function addCourses(courses) {
+	var $badges = $('#badges');
+
+	courses.forEach(function(course){
+		var $course = $('<div />', {
+			'class': 'course'
+		}).appendTo($badges);
+
+		$('<h3 />', {
+			text: course.title
+		}).appendTo($course);
+
+		$('<img />', {
+			src: course.badge
+		}).appendTo($course);
+
+		$('<a />', {
+			'class': 'btn btn-primary',
+			target: '_blank',
+			href: course.url,
+			text: 'See Course'
+		}).appendTo($course);
+	});
+}
